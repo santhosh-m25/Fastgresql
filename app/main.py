899a2d84d9c91a93eef0b134f9ae2ad0409001a0
Santhosh <santhosh.m@goml.io>
2025-07-09 15:34:25 +0530

    added routes to the main app

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from .db import engine
+from .models import SQLModel
+from .routes import user_routes, task_routes
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+app.include_router(user_routes.router)
+app.include_router(task_routes.router)
